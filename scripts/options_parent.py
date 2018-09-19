5fb2549d60db4e046d8fd90ac43607f766debe12
robertrottermann <robert@redcor.ch>
2018-09-19 16:56:08 +0200

    WIP:

--- scripts/options_parent.py
+++ scripts/options_parent.py
@@ -16,13 +16,13 @@ def add_options_parent(parser, result_dic = {}):
     parent_parser.add_argument(
         "-n", "--name",
         action="store", dest="name", default=False,
-        help = 'name of the site to create'
+        help='name of the site to create'
     )
     
     parent_parser.add_argument(
         "-F", "--force",
         action="store_true", dest="force", default=False,
-        help = """force. this parameter is used to force setting of new keys into the configuration
+        help="""force. this parameter is used to force setting of new keys into the configuration
             or when copying sitedata without disturbing a running odoo"""
     )
     
@@ -44,25 +44,25 @@ def add_options_parent(parser, result_dic = {}):
     parent_parser.add_argument(
         "-N", "--norefresh",
         action="store_true", dest="norefresh", default=False,
-        help = 'do not refresh local data, only update database with existing dump, this is the reverse of -nupdb'
+        help='do not refresh local data, only update database with existing dump, this is the reverse of -nupdb'
     )
     parent_parser.add_argument(
         "-nupdb", "--noupdatedb",
         action="store_true", dest="noupdatedb", default=False,
-        help = 'do not update local database, only update local data from remote site, this is the reverse of -N'
+        help='do not update local database, only update local data from remote site, this is the reverse of -N'
     )
     # parent_parser.add_argument(
     #     "-skip", "--skipown",
     #     action="store", dest="skipown",
-    #     help = 'provide a comma separated (no space) list of add ons to skip. used in conjuction with all.'
+    #     help='provide a comma separated (no space) list of add ons to skip. used in conjuction with all.'
     # )
     parent_parser.add_argument(
         "-ip", "--use-ip",
         action="store", dest="use_ip",
-        help = 'use the ip provided as SOURCE instead of the one found in the site description'
+        help='use the ip provided as SOURCE instead of the one found in the site description'
     )
     parent_parser.add_argument(
         "-ipt", "--use-ip-target",
         action="store", dest="use_ip_target",
-        help = 'use the ip provided to write the TARGET instead of localhost'
+        help='use the ip provided to write the TARGET instead of localhost'
     )    
--- scripts/options_remote.py
+++ scripts/options_remote.py
@@ -11,21 +11,21 @@ def add_options_remote(parser, result_dic):
     parser_remote.add_argument(
         "--add-apache",
         action="store_true", dest="add_apache", default=False,
-        help = 'add apache.conf to the apache configuration. Name must be provided',
-        need_name = True,
-        name_valid = True,
+        help='add apache.conf to the apache configuration. Name must be provided',
+        need_name=True,
+        name_valid=True,
     )
     parser_remote.add_argument(
         "--add-nginx",
         action="store_true", dest="add_nginx", default=False,
-        help = 'add configuration to the list of nginx configurations. Name must be provided',
-        need_name = True,
-        name_valid = True,
+        help='add configuration to the list of nginx configurations. Name must be provided',
+        need_name=True,
+        name_valid=True,
     )
     parser_remote.add_argument(
         "-cert", "--create-certificate",
         action="store", dest="create_certificate",
-        help = 'create or update lets encrypt ssl certificate. Name must be provided',
-        need_name = True,
-        name_valid = True,
+        help='create or update lets encrypt ssl certificate. Name must be provided',
+        need_name=True,
+        name_valid=True,
     )
